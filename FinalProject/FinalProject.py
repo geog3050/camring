de6a51789611d51bb36dbc34d93fe7e94d2c3958
camring <60206642+camring@users.noreply.github.com>
2020-05-14 10:24:49 -0500

    Project Work 5/14

--- FinalProject/FinalProject.py
+++ FinalProject/FinalProject.py
@@ -40,24 +40,30 @@ field_name = "dev_lev"
 field_type = "TEXT"
 arcpy.AddField_management(inTable, field_name, field_type)
 
-#sort above or below 50000 pop into new fields using update cursor
+#Third field for change in unemployment
+field_name2 = "unem_change"
+field_type2 = "DOUBLE"
+arcpy.AddField_management(inTable, field_name2, field_type2)
+
+#sort above or below 50000 pop & change of unem_rate into new fields using update cursor
 fc = "C:\\Users\\Cam\\Documents\\GitHub\\camring\\FinalProject\\final.gdb\\census_2010_county_DP1"
-updatecursor = arcpy.da.UpdateCursor(fc, ["DP0010001", "dev_lev"])
+#field_names = [f.name for f in arcpy.ListFields(fc)]
+#print(field_names)
+updatecursor = arcpy.da.UpdateCursor(fc, ["DP0010001", "dev_lev", "unem_rate_2010", "unem_rate_2018", "unem_change"])
 for row in updatecursor:
     if row[0] >= 50000:
         row[1] = "Urban"
     elif row[0] < 50000:
         row[1] = "Rural"
+    row[4] = row[3] - row[2]
     updatecursor.updateRow(row)
 del row
 del updatecursor
 
-#Local bivariate relationship tool to extract data from the relationship of urban and rural employment in Iowa
-dep_var = "rate_2010"
-expl_var = "DP0010001"
-out_ws = "C:\\Users\\Cam\\Documents\\GitHub\\camring\\FinalProject\\final.gdb"
-out_name = "LBR_fin"
-out_path = os.path.join(out_ws, out_name)
+#Make a layer containing positive changes in unemployment
+arcpy.MakeFeatureLayer_management("census_2010_county_DP1", "posChange_lyr", "unem_change > 0")
+#clip and make a new layer with thos values/ create a new fc from layer
+arcpy.CopyFeatures_management("posChange_lyr", "C:\\Users\\Cam\\Documents\\GitHub\\camring\\FinalProject\\final.gdb")
 
-arcpy.LocalBivariateRelationships_stats(fc, dep_var, expl_var, out_path) 
+
 
